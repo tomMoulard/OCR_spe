8129ed3e3d3bee83958e32a9953c96eb98dac834
Matthieu Moatti <matthieu.moatt@epita.fr>
2016-10-18 01:54:39 +0200

    cut fini, rsla fini

--- matrix_op/main.c
+++ matrix_op/main.c
@@ -6,7 +6,8 @@
 # include <stdlib.h>
 # include <time.h>
 
-# include "split.h"
+# include "cut.h"
+# include "rlsa.h"
 
 # define MAXVAL 100
 # define STEP 10
@@ -38,7 +39,7 @@ void matrix_print(unsigned **matrix, size_t x,size_t y)
  for (size_t i = 0; i < x; ++i) {
    array_print(matrix[i], y);
  }
- printf("|\n");
+ printf("\n");
 }
 
 /*
@@ -58,18 +59,9 @@ int main()
     array_random_fill(*(matrix + i),y);
   }
   matrix_print(matrix,x,y);
+  unsigned **mat = rlsa(matrix,x,y,1,1);
+  matrix_print(mat,x,y);
 
-  struct Matrix_double mats;
-  mats =  split_x(matrix , x, y, 5);
-
-
-  struct Triplet mat1 = mats.matrix1;
-  struct Triplet mat2 = mats.matrix2;
-
-  printf("%zu\n", mat1.x);
-  printf("%zu\n", mat1.y);
-  matrix_print(mat1.matrix,mat1.x -1,mat1.y -1);
-  matrix_print(mat2.matrix,mat2.x -1,mat2.y -1);
 
 
   //free(array);
