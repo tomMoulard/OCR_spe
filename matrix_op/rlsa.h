52999e9c716429f632f388c355163378d6b5b650
Matthieu Moatti <matthieu.moatt@epita.fr>
2016-10-18 18:07:34 +0200

    useless push

--- matrix_op/rlsa.h
+++ matrix_op/rlsa.h
@@ -6,12 +6,19 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+struct Rect{
+  size_t x1;
+  size_t y1;
+  size_t x2;
+  size_t y2;
+};
+
 unsigned** horizontal(unsigned **matrix,size_t x,size_t y,int coef);
 
 unsigned** vertical(unsigned **matrix,size_t x,size_t y,int coef);
 
 unsigned** rlsa(unsigned **matrix, size_t x, size_t y ,int coefh,int coefv);
 
-
+size_t* blockdetect(unsigned **matrix,size_t x ,size_t y);
 
 # endif
