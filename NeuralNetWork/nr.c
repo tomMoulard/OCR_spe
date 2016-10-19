cf0aa228b481c03e37155b245aaf21d0165a54d4
tomMoulard <tom.moulard@epita.fr>
2016-10-19 17:37:43 +0200

    day2

--- NeuralNetWork/nr.c
+++ NeuralNetWork/nr.c
@@ -4,6 +4,7 @@
 #include <err.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 
 int xor(int a, int b)
 {
@@ -13,29 +14,70 @@ int xor(int a, int b)
 //ulitity
 double sigmoid(double z)
 {
-	return 1 / (1 + exp(z));
+	return 1.0 / (1.0 + exp(z));
 }
 
 /*
 	Making a function to create a new neural network in a list(Array)
-	Array[0] = nb layers
-	Array[1] = size
-	Array[2] = biases
+	Array[0] = biasesDeb
+	Array[1] = weightDeb
+	Array[2] = input aka array (no uppercase)
 */
-/*
-int makeNetWork(int len, int *array)
+int **makeNetWork(int len, int *array)
 {
-
+	int **Array = malloc(5);
+	int *biasesDeb = malloc(len); 
+	int *tmp = biasesDeb;
+	//random :
+	srand(time(NULL));
+	for(; tmp < biasesDeb + len; ++tmp)
+	{
+		*tmp = rand()%2; // init biase value
+	}
+	//free(tmp);
+	int *weightDeb = malloc(len);
+	tmp = weightDeb;
+	for(; tmp < weightDeb + len; ++tmp)
+	{
+		*tmp = rand()%2; // init biase value
+	}
+	*(Array+0) = biasesDeb; //biasesFin = biasesDeb + len;
+	*(Array+1) = weightDeb; //weightFin = weightDeb + len;
+	*(Array+2) = array;	
+	return Array;	
 }
-*/
 
+int **dot(int *a, int *b, int leni, int lenj)
+{
+	int *resj = malloc(lenj * sizeof(int));
+	int **res = malloc(leni * sizeof(resj));
+	for (int i = 0; i < leni; ++i)
+	{
+		for (int j = 0; j < lenj; ++j)
+		{
+			
+		}
+	}
+	return res;
+}
 
 //convert A char to int : A = strtoul(A, NULL, 10);
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
 		errx(1, "Please input a valid input :\n	- number of pixel in width\n 	- list of pixels\n");
-	printf("%f\n", sigmoid(0.5));;
+	int *net = malloc(3 * sizeof (int));
+	//nb neurons for each layers
+	*net     = 1; //first layer
+	*(net+1) = 1; //second layer
+	*(net+2) = 1; //third layer
+	int len = 3;
+	int **test = makeNetWork(len, net);
+	int a;
+	for (int b = 0, w = 0; b < len; ++b)
+	{
+		/* code */
+	}
 	argv[0]++;
 	return 0;
 }
