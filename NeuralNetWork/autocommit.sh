a0ec01ffde790d35c97124c65c7fd86464c48f44
tomMoulard <tom.moulard@epita.fr>
2016-10-30 16:42:42 +0100

    yolo42

--- NeuralNetWork/autocommit.sh
+++ NeuralNetWork/autocommit.sh
@@ -2,7 +2,7 @@ banner commiting
 make clean
 cd ~/epita/ocr/OCR_spe/
 git status
-git add ~/epita/ocr/OCR_spe/NeuralNetWork/
+git add ~/epita/ocr/OCR_spe/NeuralNetWork/*
 git commit -m "$1"
 git push -u origin origin/neuralNetwork
 cd ~/epita/ocr/OCR_spe/NeuralNetWork/
