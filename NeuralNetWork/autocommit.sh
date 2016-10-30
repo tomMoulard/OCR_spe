cb0b2d4c1e320f55b05764a90472319d1424d955
tomMoulard <tom.moulard@epita.fr>
2016-10-30 16:07:58 +0100

    remote

--- NeuralNetWork/autocommit.sh
+++ NeuralNetWork/autocommit.sh
@@ -4,7 +4,7 @@ cd ~/epita/ocr/OCR_spe/
 git status
 git add ~/epita/ocr/OCR_spe/NeuralNetWork/
 git commit -m "$1"
-git push -u origin NeuralNetWork
+git push -u origin origin/neuralNetwork
 cd ~/epita/ocr/OCR_spe/NeuralNetWork/
 banner commited
 make
