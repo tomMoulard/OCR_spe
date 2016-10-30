90ab19d95672b2642dbc5a806f8f2547a25fe1d2
tomMoulard <tom.moulard@epita.fr>
2016-10-30 14:17:25 +0100

    no more warning plz YOLO

--- NeuralNetWork/autocommit.sh
+++ NeuralNetWork/autocommit.sh
@@ -2,7 +2,7 @@ banner commiting
 make clean
 cd ~/epita/ocr/OCR_spe/
 git add ~/epita/ocr/OCR_spe/NeuralNetWork/
-git commit -m date+" : $1"
+git commit -m "$1"
 git push 
 cd ~/epita/ocr/OCR_spe/NeuralNetWork/
 banner commited
