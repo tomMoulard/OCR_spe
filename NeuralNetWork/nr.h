54ad041fe50643fa07393fad6cb075187a0de948
tomMoulard <tom.moulard@epita.fr>
2016-11-16 17:40:30 +0100

    coucou

--- NeuralNetWork/nr.h
+++ NeuralNetWork/nr.h
@@ -13,9 +13,9 @@ struct Network{
   int    *sizes;      //list of bite of the letter
   int     len;        //len of size
   double *biases;     //list of biases for neurons
-  int     lenBiases;  //nb neurons
+  int     lenbiases;  //nb neurons
   double *weight;     //list of weights for neurons
-  int     lenWeight;  //nb neurons //useless len biases == lenweights
+  int     lenweight;  //nb neurons //useless len biases == lenweights
 };
 
 typedef struct Bashint Bashint;
